4ac0a4eab937e8addfca41d1b92a675e3ae861ba
Sonal Jain <sonaljain.mcs@gmail.com>
2020-01-31 22:26:21 -0600

    Adding My RoboFriends App to my GitHub

---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+import React , { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import {robots}  from '../robots'
+import Scroll from '../components/Scroll'
+import ErrorBoundary from '../components/ErrorBoundary'
+import './App.css'
+
+
+
+//To use States, we need to create Class Components
+class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			robots: [],
+			searchField:''
+		}
+	}
+
+	componentDidMount(){
+		fetch(`https://jsonplaceholder.typicode.com/users`)
+		.then( response => response.json())
+		.then(users =>this.setState({robots: users}))	
+	}
+
+	whenMySearchChanges = (event)=>{
+		this.setState({searchField: event.target.value})
+	}
+	render(){
+			const filteredRobots = this.state.robots.filter(robot =>{
+				return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+					})
+			return (
+			<div className ='tc'>
+				<h1 className = 'f1'>RoboFriends</h1>
+				<SearchBox searchChange = {this.whenMySearchChanges}/>
+				<Scroll>
+				<ErrorBoundary>
+					<CardList robots = {filteredRobots}/>
+				</ErrorBoundary>	
+				</Scroll>
+			</div>
+		);
+	}
+}
+
+export default App;
